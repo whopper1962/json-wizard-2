b0c47ae138a80fb44557b15692ba940d7c77c020
Masashi Kawakami <dlold1962@gmail.com>
2023-01-07 17:55:28 +0900

    add: directive / v-click-outside, v-focus

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import router from './router';
 import store from './store';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ClickOutside from 'vue-click-outside';
+
+Vue.directive('click-outside', ClickOutside);
+
+Vue.directive('focus', {
+  inserted: function (el) {
+    el.focus()
+  }
+});
 
 Vue.config.productionTip = false;
 
