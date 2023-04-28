9b9ace91b1c16a3bd8db68c463c9ce346b99cf0c
Masashi Kawakami <dlold1962@gmail.com>
2023-04-28 09:16:41 +0900

    add: multiple json generator function

--- src/lib/json-wizard/xlsx-to-json/index.js
+++ src/lib/json-wizard/xlsx-to-json/index.js
@@ -3,12 +3,21 @@ const JsonGenerator = require('@/lib/json-wizard/xlsx-to-json/json-generator');
 module.exports = function (xlsxObj) {
   const jsonGenerator = new JsonGenerator({...xlsxObj});
   if (jsonGenerator.externalTabs.length > 0) {
-    // multiple conversion
-    // refering external tabs
-    jsonGenerator.generate();
-  } else {
-    jsonGenerator.generate();
+    return generateMultipleJson(jsonGenerator);
   }
+  return generateJson(jsonGenerator);
+};
+
+// multiple conversion
+// refering external tabs
+function generateMultipleJson (jsonGenerator) {
+  console.error('External:', jsonGenerator.externalTabs);
+  return generateJson(jsonGenerator);
+}
+
+// single conversion
+function generateJson (jsonGenerator) {
+  jsonGenerator.generate();
   jsonGenerator.checkDuplicates();
   if (jsonGenerator.isArray) {
     jsonGenerator.convertToArray();
@@ -25,4 +34,4 @@ module.exports = function (xlsxObj) {
     };
   }
   return jsonGenerator.json;
-};
+}
