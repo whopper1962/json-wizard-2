61a1a6571996040b23aa53e13df9486a3943757d
Masashi Kawakami <dlold1962@gmail.com>
2023-05-14 10:53:28 +0900

    add: save current data

--- src/components/Share/ShareModal.vue
+++ src/components/Share/ShareModal.vue
@@ -2,14 +2,16 @@
   <Modal
     :name="`share-modal`"
     :clickToClose="true"
-    :onClickOkButton="execShare"
+    :onClickOkButton="execSave"
   >
     <template slot="modal-header">
       <div class="modal-title">
         <h3>Share your current data with teammates</h3>
       </div>
     </template>
-    <template slot="modal-body"></template>
+    <template slot="modal-body">
+      <input type="text" placeholder="Filename" class="form form-control" v-model="fileName" />
+    </template>
   </Modal>
 </template>
 
@@ -18,11 +20,22 @@ import Modal from '../ModalTemplate.vue';
 export default {
   components: { Modal },
   data () {
-    return {};
+    return {
+      fileName: ''
+    };
   },
   methods: {
-    execShare () {
+    execSave () {
       console.error('Share!');
+      const stateString = JSON.stringify(this.$store.state);
+      let blob = new Blob([stateString], { type: 'text/plain' });
+      let blobURL = window.URL.createObjectURL(blob);
+      let obj = document.createElement('a');
+      obj.href = blobURL;
+      obj.download = this.fileName ? `${this.fileName}.txt` : `json-wizard.txt`;
+      document.body.appendChild(obj);
+      obj.click();
+      obj.parentNode.removeChild(obj);
     }
   }
 }
